d178feff5bb4e028e3304372d03abd0779d072a7
yukuji <liane.jbm@gmail.com>
2023-06-28 08:58:10 -0600

    Initial Commit

---
+++ lianeApp/lianeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  lianeAppApp.swift
+//  lianeApp
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct lianeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
